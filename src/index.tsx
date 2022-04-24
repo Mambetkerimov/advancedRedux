f2aecc783bc772b9309297977a12f910b0d3d0b3
Alibek <mambetkerimovalibek7@gmail.com>
2022-04-24 22:13:18 +0600

    implemented count reducer and provided app

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import {Provider} from "react-redux";
+import {setupstore} from "./store/store";
+
+const store = setupstore()
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <Provider store={store}>
+        <React.StrictMode>
+            <App />
+        </React.StrictMode>
+    </Provider>
 );
 
--- src/store/reducer/UserSlice.ts
+++ src/store/reducer/UserSlice.ts
@@ -1,23 +1,27 @@
 import {IUser} from "../../models/IUser";
-import {createSlice} from "@reduxjs/toolkit";
+import {createSlice, PayloadAction} from "@reduxjs/toolkit";
 
 interface UserState {
     users: IUser[];
     isLoading: boolean;
     error: string | null;
+    count: number;
 }
 
 const initialState: UserState = {
     users: [],
     isLoading: false,
-    error: null
+    error: null,
+    count: 0
 }
 
 export const userSlice = createSlice({
     name: "user",
     initialState,
     reducers: {
-
+        increment(state, action: PayloadAction<number>) {
+            state.count += action.payload;
+        }
     }
 })
 
