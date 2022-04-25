62af2ed57faa5305c68d9157f4aad33782d0abc4
Alibek <mambetkerimovalibek7@gmail.com>
2022-04-25 22:49:53 +0600

    changed reducers on extra reducers

--- src/store/reducer/ActionCreators.ts
+++ src/store/reducer/ActionCreators.ts
@@ -2,17 +2,30 @@ import {AppDispatch} from "../store";
 import axios from "axios";
 import {IUser} from "../../models/IUser";
 import {userSlice} from "./UserSlice"
+import {createAsyncThunk} from "@reduxjs/toolkit";
 
-export const fetchUsers = () => async (dispatch: AppDispatch) => {
-    try {
-        dispatch(userSlice.actions.usersFetching())
-        const response = await axios.get<IUser[]>('https://jsonplaceholder.typicode.com/users')
-        dispatch(userSlice.actions.usersFetchingSuccess(response.data))
-    } catch (error) {
-        let errorMessage = "Some error text";
-        if(error instanceof Error) {
-            errorMessage = error.message
+// export const fetchUsers = () => async (dispatch: AppDispatch) => {
+//     try {
+//         dispatch(userSlice.actions.usersFetching())
+//         const response = await axios.get<IUser[]>('https://jsonplaceholder.typicode.com/users')
+//         dispatch(userSlice.actions.usersFetchingSuccess(response.data))
+//     } catch (error) {
+//         let errorMessage = "Some error text";
+//         if(error instanceof Error) {
+//             errorMessage = error.message
+//         }
+//         dispatch(userSlice.actions.usersFetchingError(errorMessage));
+//     }
+// }
+
+export const fetchUsers = createAsyncThunk(
+    'user/fetchAll',
+    async(_, thunkAPI) => {
+        try {
+            const response = await axios.get<IUser[]>('https://jsonplaceholder.typicode.com/users')
+            return response.data
+        } catch (e) {
+            return thunkAPI.rejectWithValue("Some error text")
         }
-        dispatch(userSlice.actions.usersFetchingError(errorMessage));
     }
-}
+)
